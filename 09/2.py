cfbc490adfc5f37f53c1bbb20cf1adba35bf3af7
Aleksei Grinko <als.grinko@yandex.ru>
2023-05-29 19:52:54 +0400

    9 - 2

---
+++ 09/2.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+size = (5, 5)
+numbers = np.random.randint(1, 3, size)
+count = np.count_nonzero(np.corrcoef(numbers) == 1.0)
+print(f'Представлен двумерный массив 5 х 5: \n {numbers}')
+if (np.array(numbers).__len__() < count):
+    print('В массиве имеются одинаковые строки')
+else:
+    print('В массиве нет одинаковых строк')
+
+#print(f'Представлен список элементов: \n {numbers} \n В нем {count} уникальных элементов: \n {uniq_elements}')
